6c4c0029001dafa070526b00f42c7297bb9dba03
NaroaMS04 <150126183+NaroaMS04@users.noreply.github.com>
2023-12-11 22:55:48 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ import express from 'express';
 import mustacheExpress from 'mustache-express';
 import bodyParser from 'body-parser';
 import { __dirname } from './dirname.js';
-import boardRouter from './boardRouter.js';
-
-
-
+import boardRouter from './rutas.js';
 //Importamos los modulos que vamos a utilizar en app.js
 
 const app = express();//Definimos la aplicación como una función express
