1da70a9f0b4b1d889cbdef41f4265352e74f79a4
Garrobo08 <andreagarroboguzman@gmail.com>
2023-12-11 14:19:01 +0100

    seguimos con la validacion de los campos haciendo los cambios necesarios en el css y el html

--- src/form.js
+++ src/form.js
@@ -1,11 +1,43 @@
-const formulario = document.getElementById('formucolor');
-const inputs = document.querySelectorAll('#formucolor input'); //almacenamos todos los inputs de nuestro formulario
+//Estos son los elementos seleccionados
+const form = document.getElementById('formucolor');
+const deporte = document.getElementById('campo-nombre');
+const fecha = document.getElementById('campo-fecha');
+const descripcion = document.getElementById('descripcion');
+const foto = document.getElementById('foto');
+const participantes = document.getElementById('Número-de-participantes');
+const tipo = document.getElementById('tipo');
 
+//agragamos un evento
+form.addEventListener('guardar' , e  => {
+    e.preventDefault();
+    checkInputs();
+});
 
-//objetos (expresiones) que tiene dentro varias propiedades.
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    descripción: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-    img: /https:\/\/[a-zA-Z\.\/-] + //paraurl
+//metodo que valide los inputs
+function checkInputs(){
+    const deporteValue = deporte.nodeValue.trim(); //trim para eliminar cualquier valor que este vacío al principio y al final
+    const fechaValue = fecha.nodeValue.trim(); 
+    const descripcionValue = descripcion.nodeValue.trim(); 
+    const fotoValue = foto.nodeValue.trim(); 
+    const participantesValue = participantes.nodeValue.trim(); 
+    const tipoValue = tipo.nodeValue.trim(); 
+
+    if (deporteValue === ''){
+        setErrorFor
+    }
+
 }
 
+//nos muestra el error
+function setErrorFor(input, message){
+    const formControl = input.parentElement; //elemento que esta recibiendo como parametro
+    const small = formControl.querySelecto ('small');
+    formControl.className = '.formucolor error';
+    small.innerText = message;
+
+}
+
+function setSuccessFor (input){
+    const formControl = input.parentElement;
+    formControl.className = 'formucolor success';
+}
